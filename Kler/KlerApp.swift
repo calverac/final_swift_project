c4ead12eef5506f52d98c9103327f9826063ec62
Catalina Calvera <calverac@bc.edu>
2025-12-06 20:18:56 -0500

    Initial Commit

---
+++ Kler/KlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlerApp.swift
+//  Kler
+//
+//  Created by Catalina on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct KlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
